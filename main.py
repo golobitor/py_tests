ccadf1f670c70a7ee770d93063ca603949e33e26
golobitor <golobitor113rus@yandex.ru>
2025-12-05 13:35:54 +0300

    add_test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("test")
+import time
+from selenium import webdriver
+import pytest
+
+@pytest.fixture()
+def browser():
+    chrome_browser = webdriver.Chrome()
+    return chrome_browser
+
+def test_button(browser):
+    browser.get("https://id-store.ru/")
+    time.sleep(5)
+    assert browser.find_element("xpath", "//a[@href='/basket/']").is_displayed()
+
